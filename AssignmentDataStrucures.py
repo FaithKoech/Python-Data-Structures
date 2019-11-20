4b8093031701f52bd47b731e5691f1c731f0563a
FaithKoech <faithkoech1@gmail.com>
2019-11-20 17:14:17 +0300

    intro to datasets

---
+++ AssignmentDataStrucures.py
@@ -0,0 +1,7 @@
+# taskList = [23, “Jane”, [“Lesson 23”, 560, {“currency” : “KES”}], 987, (76,”John”)]
+Taks = [23, "Jane", ["Lesson 23", 560, {"Currency" : "KES"}], 987, (76,"John")]
+# 1. Determing type of variable in taskList using an inbuilt function
+print(type(Taks))
+
+
+
---
+++ DatastructuresAsiigment.py
@@ -0,0 +1,17 @@
+# taskList = [23, “Jane”, [“Lesson 23”, 560, {“currency” : “KES”}], 987, (76,”John”)]
+Taks = [23, "Jane", ["Lesson 23", 560, {"Currency" : "KES"}], 987, (76,"John")]
+# 1. Determing type of variable in taskList using an inbuilt function
+print(type(Taks))
+
+# 2. Print KES
+
+# 3. Print 560
+
+# 4. Use a function to determine the length of taksList
+x = [23, "Jane", ["Lesson 23", 560, {"Currency" : "KES"}], 987, (76,"John")]
+print(len(x))
+
+# 5. Change 987 to 789 without using an inbuilt -method or Assignment
+
+# 6.Change the name “John” to “Jane”.
+
